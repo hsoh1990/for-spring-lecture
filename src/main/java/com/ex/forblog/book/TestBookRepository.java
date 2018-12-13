4bc52b4a28ff9a655f614b173924993568b60e0c
Hyeongseok Oh <ohs4123@gmail.com>
2018-12-14 01:43:22 +0900

    spring core IoC & Bean @Profile을 통한 active profile 설정 @Component로 설정하기

--- src/main/java/com/ex/forblog/book/TestBookRepository.java
+++ src/main/java/com/ex/forblog/book/TestBookRepository.java
@@ -1,6 +1,9 @@
 package com.ex.forblog.book;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
+@Repository
+@Profile("!prod")
 public class TestBookRepository implements BookRepository{
 }
