27522a0da7e9ef27705556d1b61b0137b7c96311
Hyeongseok Oh <ohs4123@gmail.com>
2018-12-16 23:59:59 +0900

    spring core IoC & Bean property 사용방법

--- src/main/java/com/ex/forblog/AppRunner.java
+++ src/main/java/com/ex/forblog/AppRunner.java
@@ -2,6 +2,7 @@ package com.ex.forblog;
 
 import com.ex.forblog.book.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
@@ -19,11 +20,15 @@ public class AppRunner implements ApplicationRunner{
     @Autowired
     BookRepository bookRepository;
 
+    @Value("${app.about}")
+    String appAbout;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Environment environment = ctx.getEnvironment();
-        System.out.println(Arrays.toString(environment.getActiveProfiles()));
-        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
+        System.out.println(environment.getProperty("app.name"));
+        System.out.println(environment.getProperty("app.about"));
+        System.out.println(this.appAbout);
 
     }
 }
