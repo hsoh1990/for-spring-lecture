bf76c23ece3d0cb83a0e7ca562386bfe622b7728
Hyeongseok Oh <ohs4123@gmail.com>
2018-12-17 21:14:54 +0900

    spring core IoC & Bean Event @Async 를 통한 비동기적 실행

--- src/main/java/com/ex/forblog/AnotherEventHandler.java
+++ src/main/java/com/ex/forblog/AnotherEventHandler.java
@@ -1,15 +1,14 @@
 package com.ex.forblog;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnotherEventHandler {
 
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE + 2)
+    @Async
     public void handle(AppEvent event){
         System.out.println(Thread.currentThread().toString());
         System.out.println("AnotherEventHandler = " +event.getData());
--- src/main/java/com/ex/forblog/AppEventHandler.java
+++ src/main/java/com/ex/forblog/AppEventHandler.java
@@ -1,15 +1,14 @@
 package com.ex.forblog;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppEventHandler {
 
     @EventListener
-    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Async
     public void handle(AppEvent event){
         System.out.println(Thread.currentThread().toString());
         System.out.println("AppEventHandler = " +event.getData());
