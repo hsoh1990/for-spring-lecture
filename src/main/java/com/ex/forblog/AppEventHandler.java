eb80795f04e81b4778df6c4da04116de9c1952cb
Hyeongseok Oh <ohs4123@gmail.com>
2018-12-17 21:18:08 +0900

    spring core IoC & Bean Spring boot가 제공하는 Event

--- src/main/java/com/ex/forblog/AppEventHandler.java
+++ src/main/java/com/ex/forblog/AppEventHandler.java
@@ -1,5 +1,7 @@
 package com.ex.forblog;
 
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -12,5 +14,19 @@ public class AppEventHandler {
     public void handle(AppEvent event){
         System.out.println(Thread.currentThread().toString());
         System.out.println("AppEventHandler = " +event.getData());
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextRefreshedEvent event){
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextRefreshedEvent");
+    }
+
+    @EventListener
+    @Async
+    public void handle(ContextClosedEvent event){
+        System.out.println(Thread.currentThread().toString());
+        System.out.println("ContextClosedEvent");
     }
 }
