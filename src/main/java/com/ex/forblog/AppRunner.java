c53bbab29440f7804437c26321a60ea7bfe4fb7d
오형석 <ohs4123@gmail.com>
2018-12-19 00:22:02 +0900

    spring core SpEL Spring Expression Language 기본 사용법

--- src/main/java/com/ex/forblog/AppRunner.java
+++ src/main/java/com/ex/forblog/AppRunner.java
@@ -1,6 +1,7 @@
 package com.ex.forblog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.convert.ConversionService;
@@ -9,11 +10,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppRunner implements ApplicationRunner{
 
-    @Autowired
-    ConversionService conversionService;
+    @Value("#{1 + 1}")
+    int value;
+
+    @Value("#{'hello' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean trueOrFalse;
+
+    @Value("hello")
+    String hello;
+
+    @Value("${app.about}")
+    String appAbout;
+
+    @Value("#{${my.value} eq 100}")
+    boolean myValueIs100;
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService);
+        System.out.println("==============");
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(trueOrFalse);
+        System.out.println(hello);
+        System.out.println(appAbout);
+        System.out.println(myValueIs100
+        );
     }
 }
