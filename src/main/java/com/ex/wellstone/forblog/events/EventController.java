daf99c0fd87a47a841eb3626152d507e4e1d5489
오형석 <ohs4123@gmail.com>
2018-12-22 21:29:31 +0900

    spring rest api 개발 02 DTO validation

--- src/main/java/com/ex/wellstone/forblog/events/EventController.java
+++ src/main/java/com/ex/wellstone/forblog/events/EventController.java
@@ -2,13 +2,16 @@ package com.ex.wellstone.forblog.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -24,7 +27,12 @@ public class EventController {
     ModelMapper modelMapper;
 
     @PostMapping()
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto){
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
+
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
         Event event = modelMapper.map(eventDto, Event.class);
         final Event newEvent = eventRepository.save(event);
         final URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
