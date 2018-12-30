bd1599fe7f5759078961afd26722b1346b22335d
오형석 <ohs4123@gmail.com>
2018-12-30 21:26:28 +0900

    spring rest api 개발 05 spring security 폼 인증 설정

--- src/main/java/com/ex/wellstone/forblog/configs/AppConfig.java
+++ src/main/java/com/ex/wellstone/forblog/configs/AppConfig.java
@@ -1,13 +1,21 @@
 package com.ex.wellstone.forblog.configs;
 
+import com.ex.wellstone.forblog.accounts.Account;
+import com.ex.wellstone.forblog.accounts.AccountRole;
+import com.ex.wellstone.forblog.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Set;
+
 @Configuration
-public class AppConfig  {
+public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
@@ -15,8 +23,27 @@ public class AppConfig  {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                final Account hsoh = Account.builder()
+                        .email("hsoh@email.com")
+                        .password("hsoh")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(hsoh);
+            }
+        };
+
     }
 
 }
